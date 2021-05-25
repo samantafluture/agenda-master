2bbc39f0fdc7b08b3ff89a1a85156b2c34b3eccb
Samanta Fluture <samantafluture@gmail.com>
2021-05-24 23:04:28 -0300

    adding contact list to the html table

--- agenda.js
+++ agenda.js
@@ -3,11 +3,16 @@ console.log("=== AGENDA ===");
 (function () {
     console.log("Starting...");
 
+    // indicar qual o endpoint onde vc vai disparar essa comunicação; onde está o backend
+    // configuração
+    const endpoint = "http://localhost:7000/contacts";
+
     // User Interface (UI)
     const ui = {
         fields: document.querySelectorAll("input"), // ui.fields (todos os fiels com tag input)
         buttonAdd: document.querySelector(".pure-button-primary"), // ui.buttonAdd
-        contactList: document.querySelector(".pure-table") // ui.contactList
+        contactList: document.querySelector(".pure-table"), // ui.contactList
+        formTitle: document.querySelector(".message-error")
     };
 
     console.log(ui);
@@ -30,7 +35,7 @@ console.log("=== AGENDA ===");
 
             /* validação visual -> coloca e tira a classe error do css */
 
-            if (field.value !== "") {
+            if (field.value.trim() !== "") { // .tim() para limpar os espaços dentro da String
                 console.log(field.id, "Preenchido");
                 field.classList.remove("error"); // retira do css a classe de erro se o field tá preenchido!
                 data[field.id] = field.value; // conforme foi passando, foi construindo um objeto com os dados válidos
@@ -53,11 +58,107 @@ console.log("=== AGENDA ===");
 
     };
 
-    const addContact = function() {};
+    const addContact = function(contact) {
+        // console.log(arguments[0]);
+        console.log(contact); // dou nome ao argumento para pegar ele; é um parâmetro que fizemos
 
-    const getContact = function() {};
+        // passar atributos no objeto; traçar uma estratégia para falar com o endpoint
+        // objeto de configuração
+        const config = {
+            method: "POST", // estratégia de comunicação
+            body: JSON.stringify(contact),   // o que vc quer gravar, o corpo
+            headers: new Headers({  // o que precisa para identificar o que estou enviando
+                "Content-type": "application/json" // tipo de dado que estou mandando
+            }) 
+        };
 
-    const clearFields = function() {};
+        // 1º parâmetro: endpoint, onde quero fazer a requisição
+        // 2º parâmetro: configuração da comunicação
+        // fetch(endpoint, config);
+        fetch(endpoint, config)
+            // encadamento de métodos da promise api & fetch api -> tratamento de erro e sucesso
+            // passa as funções callback
+            .then(addContactSuccess)
+            .catch(addContactError);
+            // .finally() 
+    };
+
+    const showMessage = function(msg, action) {
+        ui.formTitle.textContent = msg;
+        ui.formTitle.classList[action]("message-error"); // add ou remover classe error do css 
+    };
+
+    // Success + Erro => callbacks; um ou o outro vão ser executados
+    const addContactSuccess = function() {
+        showMessage("Agenda", "remove");
+        clearFields();
+        getContacts();
+    };
+
+    const addContactError = function() {
+        showMessage("Ocorreu erro ao adicionar dados!", "add");
+    };
+
+    const getContacts = function() {
+        const config = {
+            method: "GET",
+            headers: new Headers({
+                "Content-type": "application/json"
+            })
+        };
+        
+        fetch(endpoint, config)
+            .then(getContactsSuccess) // trata na 1ª função
+                .then(getDataSuccess) // pega os dados na 2ª -> isso é só no caso do GET (são duas vezes)
+                .catch(getDataError)
+            .catch(getContactsError);
+    };
+
+    const getContactsSuccess = function(response) {
+       // console.log(arguments[0]); // para descobrir o que está chegando!
+       showMessage("Agenda", "remove");
+       return response.json(); // mesmo objeto que acima -> precisa converter pra json 
+    };
+
+    const getContactsError = function() {
+        showMessage("Ocorreu erro ao recuperar os dados!", "add");
+    };
+
+    const getDataSuccess = function(contacts) {
+        console.table(contacts); // array com os objetos que salvamos
+
+        // recuperar os dados => executar esta função para cada um dos contatos cadastrados
+        // map() => itera e retorna os dados formatados / transformados (permite q vc faça uma transformação); retorna um novo array!
+        const htmlTable = contacts.map(function(contact) {
+            console.log(contact);
+
+            // template string (html entre ``)
+            // posso colocar código js ${}
+            const lineTable = 
+                `<tr>
+                    <td>${contact.id}</td>
+                    <td>${contact.name}</td>
+                    <td>${contact.phone}</td>
+                    <td>${contact.email}</td>
+                    <td><a href="#">Excluir</a> | <a href="#">Editar</a></td>
+                </tr>`
+           console.log(lineTable);
+           return lineTable;
+        }).join("");
+
+        console.log(htmlTable);
+        ui.contactList.innerHTML = htmlTable;
+    };
+
+    const getDataError = function() {
+        showMessage("Ocorreu erro ao converter os dados!", "add");
+    };
+
+    const clearFields = function() {
+        ui.fields.forEach(function(field) {
+            field.value = "";
+        });
+    };
 
     const removeContact = function() {};
 
@@ -70,6 +171,12 @@ console.log("=== AGENDA ===");
     clicar no adicionar gera um evento de mouse */
 
     ui.buttonAdd.onclick = validateFields;
+
+    // Initialize 
+
+    /* funções para serem executas assim que a página carrega */
+
+    getContacts();
 
 
 })();
